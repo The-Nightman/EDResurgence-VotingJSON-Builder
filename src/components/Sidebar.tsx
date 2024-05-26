6ce4eb0dd064d1f91ce5ad87616a79f2e2d6ab43
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-05-26 16:52:23 +0100

    add code comments and JSDoc to Sidebar component and import barrel

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,6 +8,14 @@ interface SidebarProps {
   };
 }
 
+/**
+ * Sidebar component that displays gametypes and maps from the target directories.
+ *
+ * @param {Object} mapsVariantsData - The data containing maps and variants.
+ * @param {string[]} mapsVariantsData.maps - The maps data.
+ * @param {string[]} mapsVariantsData.variants - The variants data.
+ * @returns {JSX.Element} The rendered Sidebar component.
+ */
 export const Sidebar = ({ mapsVariantsData }: SidebarProps) => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
 
@@ -21,9 +29,14 @@ export const Sidebar = ({ mapsVariantsData }: SidebarProps) => {
         className={`fixed top-1/2 ${sidebarOpen ? "right-[17rem]" : "right-4"}`}
         aria-label={`${sidebarOpen ? "Close" : "Open"} sidebar`}
         title={`${sidebarOpen ? "Close" : "Open"} sidebar`}
+        // toggle sidebarOpen state to open and close the sidebar
         onClick={() => setSidebarOpen(!sidebarOpen)}
       >
-        {sidebarOpen ? <ArrowForwardIos fontSize="large" /> : <ArrowBackIos fontSize="large" />}
+        {sidebarOpen ? (
+          <ArrowForwardIos fontSize="large" />
+        ) : (
+          <ArrowBackIos fontSize="large" />
+        )}
       </button>
       <div
         className={`${!sidebarOpen && "hidden"} flex flex-col gap-y-2 h-full`}
@@ -31,6 +44,7 @@ export const Sidebar = ({ mapsVariantsData }: SidebarProps) => {
         <article className="flex flex-col grow h-[calc(50%-0.5rem)]">
           <h3 className="text-xl font-bold">GAMETYPES</h3>
           <div className="h-full overflow-y-scroll">
+            {/* map and display variants from mapsVariantsData received from parent */}
             {mapsVariantsData.variants.map((variant) => (
               <p key={variant}>{variant}</p>
             ))}
@@ -39,6 +53,7 @@ export const Sidebar = ({ mapsVariantsData }: SidebarProps) => {
         <article className="flex flex-col grow h-[calc(50%-0.5rem)]">
           <h3 className="text-xl font-bold">MAPS</h3>
           <div className="h-full overflow-y-scroll">
+            {/* map and display maps from mapsVariantsData received from parent */}
             {mapsVariantsData.maps.map((map) => (
               <p key={map}>{map}</p>
             ))}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
+// import components into the barrel
 import { Sidebar } from "./Sidebar";
 
+// export the components to allow for imports from one source 
+// making code more concise and easier to read
 export { Sidebar };
