dc957ec6eb26abae4d8006adaa56d4a70e25c5fb
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-05-31 00:15:34 +0100

    add OpenFolderDialog component and import barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,8 @@
 import { Sidebar } from "./Sidebar";
 import { TypeForm } from "./TypeForm";
 import { DialogFoundation } from "./DialogFoundation";
+import { OpenFolderDialog } from "./OpenFolderDialog";
 
 // export the components to allow for imports from one source 
 // making code more concise and easier to read
-export { Sidebar, TypeForm, DialogFoundation };
+export { Sidebar, TypeForm, DialogFoundation, OpenFolderDialog };
