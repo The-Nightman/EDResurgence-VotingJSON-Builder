7954eda18c728e4fc39a83bf810cbf39658ee5db
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-06-01 23:42:01 +0100

    add SettingsDialog component

---
+++ src/components/SettingsDialog.tsx
@@ -0,0 +1,118 @@
+import { useContext } from "react";
+import { SettingsContext } from "../contexts/SettingsContext";
+
+interface SettingsDialogProps {
+  onResolve: () => void;
+}
+
+/**
+ * Renders a settings dialog component.
+ *
+ * @param {SettingsDialogProps} props - The component props.
+ * @param {Function} props.onResolve - The callback function to resolve the blocking promise.
+ * @returns {JSX.Element} The rendered settings dialog component.
+ */
+export const SettingsDialog = ({ onResolve }: SettingsDialogProps) => {
+  const { settings, setSettings } = useContext(SettingsContext);
+
+  /**
+   * Handles the click event for the button.
+   * Invokes the "electron-store-set-cfg" method of the window.ipcRenderer object
+   * to set the settings in the electron store, and then calls the onResolve function.
+   */
+  const handleClick = async () => {
+    await window.ipcRenderer.invoke("electron-store-set-cfg", settings);
+    onResolve();
+  };
+
+  return (
+    <div
+      role="alertdialog"
+      aria-label="Save Files Dialog"
+      className="flex flex-col h-full min-w-[60%] w-min my-4 mx-auto"
+    >
+      <h2 className="text-5xl">SETTINGS</h2>
+      <div className="flex flex-col gap-6 text-xl text-white [&_select]:text-black [&_input]:text-black [&_select]:bg-[#a3bbd8] [&_input]:bg-[#a3bbd8] [&_select]:font-sans [&_input]:font-sans [&_select]:rounded [&_input]:rounded">
+        <label className="flex flex-col">
+          Background
+          <select
+            name="Background select"
+            id="background"
+            defaultValue={settings.background}
+            onChange={(e) =>
+              setSettings({ ...settings, background: e.target.value })
+            }
+          >
+            <option value="forgeBackground">Forge</option>
+            <option value="test">...</option>
+          </select>
+        </label>
+        <label className="flex flex-col">
+          Volume: {Math.round(settings.volume * 100)}%
+          <input
+            type="range"
+            name="Volume"
+            id="volume"
+            min={0}
+            max={1}
+            defaultValue={settings.volume}
+            step={0.01}
+            onChange={(e) =>
+              setSettings({
+                ...settings,
+                volume: parseFloat(e.target.value),
+              })
+            }
+          />
+        </label>
+        <div className="flex flex-col">
+          <label>
+            <input
+              className="mr-1"
+              type="checkbox"
+              name="High contrast text"
+              id="highContrastText"
+              defaultChecked={settings.highContrastText}
+              onChange={(e) =>
+                setSettings({ ...settings, highContrastText: e.target.checked })
+              }
+            />
+            High contrast text
+            {settings.highContrastText ? " (Enabled)" : " (Disabled)"}
+          </label>
+          <span className="text-4xl text-[#aac0da]">
+            aAbBcC/<span className="text-white">aAbBcC</span>
+          </span>
+        </div>
+        <label>
+          <input
+            className="mr-1"
+            type="checkbox"
+            name="High contrast text"
+            id="highContrastText"
+            disabled
+          />
+          Advanced map options (Coming soon)
+        </label>
+      </div>
+      <div className="relative flex h-7 mt-auto">
+        <button
+          className="hover:animate-pulse flex-grow hover:bg-[#963E15] active:bg-[#53220C]"
+          title="Close Dialog"
+          aria-label="Close Dialog"
+          onClick={handleClick}
+        >
+          <span className="sr-only">OK</span>
+        </button>
+        <span
+          aria-hidden
+          // pointer-events-none prevents text selection and click events allowing
+          // the button to function seamlessly as the only clickable element
+          className="absolute left-1/2 text-xl -translate-x-1/2 pointer-events-none"
+        >
+          OK
+        </span>
+      </div>
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ import { TypeForm } from "./TypeForm";
 import { DialogFoundation } from "./DialogFoundation";
 import { OpenFolderDialog } from "./OpenFolderDialog";
 import { SaveFilesDialog } from "./SaveFilesDialog";
+import { SettingsDialog } from "./SettingsDialog";
 
 // export the components to allow for imports from one source
 // making code more concise and easier to read
@@ -13,4 +14,5 @@ export {
   DialogFoundation,
   OpenFolderDialog,
   SaveFilesDialog,
+  SettingsDialog,
 };
