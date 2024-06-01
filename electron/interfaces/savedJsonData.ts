13d8f203e4592cef4e38b5e9e7993084c985ac6e
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-06-01 18:12:19 +0100

    add interfaces and import barrel for userConfig and savedJsons data stores

---
+++ electron/interfaces/savedJsonData.ts
@@ -0,0 +1,24 @@
+interface MapObj {
+  displayName: string;
+  mapName: string;
+}
+
+interface TypeObj {
+  id?: number;
+  displayName: string;
+  typeName: string;
+  specificMaps: MapObj[];
+  modPack?: string;
+  randomChance?: number;
+  commands?: string[];
+  endOfMatchCommands?: string[];
+}
+
+export interface SavedJsonData {
+  name: string;
+  date: number;
+  data: {
+    maps: MapObj[];
+    types: TypeObj[];
+  };
+}
